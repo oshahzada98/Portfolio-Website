f17342b6e1aae3ce77a3279f945b8309031e4ee2
Owais Shahzada <shahzadaoa@gmail.com>
2020-12-21 20:36:26 -0500

    First Commit

---
+++ colorChanger.js
@@ -0,0 +1,38 @@
+console.log("It's Working")
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+    setTheme('light')
+}
+else{
+    setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+for (var i=0; themeDots.length > i; i++){
+    themeDots[i].addEventListener('click', function(){
+        let mode = this.dataset.mode 
+        console.log('Option clicked', mode)
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode){
+    if(mode == 'light'){
+        document.getElementById("theme-style").href= '/Users/oshahzada98/Desktop/Portfolio Website/default.css'
+    }
+    if(mode == 'blue'){
+        document.getElementById("theme-style").href= '/Users/oshahzada98/Desktop/Portfolio Website/blue.css'
+    }
+    if(mode == "green"){
+        document.getElementById("theme-style").href= '/Users/oshahzada98/Desktop/Portfolio Website/green.css'
+    }
+    if(mode == "purple"){
+        document.getElementById("theme-style").href= '/Users/oshahzada98/Desktop/Portfolio Website/purple.css'
+    }
+
+    localStorage.setItem('theme', mode)
+
+}
